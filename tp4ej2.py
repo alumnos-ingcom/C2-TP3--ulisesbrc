26fc41f3165afae6c0b4498667bb12266365221b
Ulises <ulises4@gmail.com>
2021-05-27 23:07:25 -0300

    Funciona

---
+++ tp4ej2.py
@@ -0,0 +1,27 @@
+################
+# Walter Ulises Ayllapan - @ulisesbrc
+# Ejercicio 1. Ingreso de números enteros
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+def suma_lenta(numero, otro_numero):
+    """
+    Esta funcion hace la suma lenta entre dos números.
+    """
+    if(numero>=0):
+        while(numero!=0):
+            numero=numero-1
+            otro_numero=otro_numero+1
+        return otro_numero
+    else:
+        while(numero!=0):
+            numero=numero+1
+            otro_numero=otro_numero-1
+        return otro_numero
+
+
+if __name__ == "__main__":
+    suma = suma_lenta(-4,-44)
+    print(suma)
