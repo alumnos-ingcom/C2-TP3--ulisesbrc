c2336a0284a0f880f1afcdf32dbb42e81f62ab0f
Ulises <ulises4@gmail.com>
2021-05-28 02:42:15 -0300

    Funciona

---
+++ tp4ej5.py
@@ -0,0 +1,27 @@
+################
+# Walter Ulises Ayllapan - @ulisesbrc
+# Ejercicio 5. Números positivos y negativos
+# UNRN Andina - Introducción a la Ingenieria en Computación
+################
+
+
+# Reemplazar por las funciones del ejercicio
+def signo(numero):
+    """
+    Esta función recibe un número e indica si el mismo es positivo, negativo o cero.
+    """
+    if(numero>0):
+        return "Número positivo"
+    else:
+        if(numero<0):
+            return "Número negativo"
+        else:
+            return "Número cero"
+
+if __name__ == "__main__":
+    numero = signo(4);
+    print(f'el 4 es {numero}')
+    numero = signo(-4);
+    print(f'el -4 {numero}')
+    numero = signo(0);
+    print(f'el 0 {numero}')
