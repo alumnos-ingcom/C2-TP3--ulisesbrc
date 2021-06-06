b96f06a0d001d3afd4a6771bc70c92dfa8255228
Ulises <ulises4@gmail.com>
2021-06-06 15:33:21 -0300

    Agregue la funcion prueba y le agregue interactividad

--- tp4ej4.py
+++ tp4ej4.py
@@ -3,7 +3,18 @@
 # Ejercicio 4. Comparación de números
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
+def ingrese_numero(mensaje):
+    """
+    Esta funcion muestra un mensaje y agrega la # para indicar el ingreso
+    de un número entero.
+    """
+    numero= False
+    try:
+        ingreso = input(mensaje + " #")
+        numero = float(ingreso)
+        return numero  
+    except ValueError as error:
+        print("No era un número")
 
 # Reemplazar por las funciones del ejercicio
 def compara(numero, otro_numero):
@@ -20,11 +31,27 @@ def compara(numero, otro_numero):
             return -1
         else:
             return 0
-
+def prueba():
+    numero = False
+    while not numero:
+        try:
+            numero=ingrese_numero("Ingresa un número")
+        except:
+            print("El valor ingresado no es correcto")
+    otro_numero=False
+    while not otro_numero:
+        try:
+            otro_numero=ingrese_numero("Ingresa otro número")
+        except:
+            print("El valor ingresado no es correcto")        
+    salida = compara(numero,otro_numero)
+    if (salida == 1):
+        print(f"el numero {numero} es mayor a {otro_numero}")
+    else:
+        if (salida == -1):
+            print(f"el numero {numero} es menor a {otro_numero}")
+        else:
+            print(f"el numero {numero} es igual a {otro_numero}")    
+    
 if __name__ == "__main__":
-    salida = compara(9,6);
-    print(f'9 > 6 salida {salida}')
-    salida = compara(6,9);
-    print(f'6 < 9 salida {salida}')
-    salida = compara(9,9);
-    print(f'9=9 salida {salida}')
+    prueba()  
