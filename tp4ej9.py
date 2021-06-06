8174705721de23d8d7061cced51b2c940c0a4cea
Ulises <ulises4@gmail.com>
2021-06-06 17:21:09 -0300

    Agregué la funcion prueba y agregué interactividad @martinvilu

--- tp4ej9.py
+++ tp4ej9.py
@@ -6,6 +6,7 @@
 
 
 # Reemplazar por las funciones del ejercicio
+from tp4ej1 import ingreso_entero
 def es_primo(numero):
     if numero<=1:
         return False
@@ -13,6 +14,19 @@ def es_primo(numero):
         if numero%n==0:
             return False
     return True
-
+def prueba():
+    numero = False
+    mensaje="Ingrese un número"
+    mensaje_error = "Ingrese un número válido"
+    while not numero:
+        try:
+            numero = ingreso_entero(mensaje)
+        except:
+            print(mensaje_error)
+    esprimo = es_primo(numero)
+    if esprimo:
+        print(f"El número {numero} es primo")
+    else:
+        print(f"El número {numero} no es primo")
 if __name__ == "__main__":
-   print(es_primo(79))
+    prueba()
