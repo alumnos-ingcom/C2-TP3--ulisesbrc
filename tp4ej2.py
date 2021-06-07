8db822fbedb73e941c7809265da11eeaec976b3d
Ulises <ulises4@gmail.com>
2021-06-06 21:30:20 -0300

    Agregué interactividad e importé la funcion ingreso_entero del ej1 @martinvilu

--- tp4ej2.py
+++ tp4ej2.py
@@ -3,7 +3,7 @@
 # Ejercicio 2. Suma lenta
 # UNRN Andina - Introducción a la Ingenieria en Computación
 ################
-
+from tp4ej1 import ingreso_entero
 
 # Reemplazar por las funciones del ejercicio
 def suma_lenta(numero, otro_numero):
@@ -21,7 +21,22 @@ def suma_lenta(numero, otro_numero):
             otro_numero=otro_numero-1
         return otro_numero
 def prueba():
-    suma = suma_lenta(-4,-44)
-    print(suma)
+    numero=False
+    otro_numero=False
+    mensaje="Ingrese un número"
+    mensaje_otro="Ingrese otro número"
+    mensaje_error="No es un número válido, intente de nuevo"
+    while not numero:
+        try:
+            numero=ingreso_entero(mensaje)
+        except:
+            print(mensaje_error)
+    while not otro_numero:
+        try:
+            otro_numero=ingreso_entero(mensaje_otro)
+        except:
+            print(mensaje_error)    
+    suma = suma_lenta(numero,otro_numero)
+    print(f"La suma lenta entre {numero} y {otro_numero} es {suma}")
 if __name__ == "__main__":  
     prueba()
