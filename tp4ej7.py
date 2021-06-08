5173659add7dc6f82010df9fc60f518ae6fc90d9
Ulises <ulises4@gmail.com>
2021-06-07 21:06:17 -0300

    Saqué la reutilización y agregué otra variable @martinvilu

--- tp4ej7.py
+++ tp4ej7.py
@@ -15,17 +15,19 @@ def division_lenta(dividendo, divisor):
     return (cociente,resto)
 
 def prueba():
-    dividendo = False
-    divisor = False
+    solicitar = True
     mensaje_error = "Ingrese un número válido"
-    while not dividendo:
+    while solicitar:
         try:
             dividendo = ingreso_entero("Ingrese un número entero a ser dividido")
+            solicitar=False
         except:
             print(mensaje_error)
-    while not divisor:
+    solicitar= True
+    while solicitar:
         try:
             divisor = ingreso_entero(f"Ingrese un número entero que dividirá a {dividendo}")
+            solicitar=False
         except:
             print(mensaje_error)
     divisionlenta = division_lenta(dividendo ,divisor)
